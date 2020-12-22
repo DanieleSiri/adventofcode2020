4adcdf5f8e11bfc4921c38988ae52f81ed6c427f
Daniele Siri <daniele.siri97@gmail.com>
2020-12-22 19:03:27 +0100

    day 16 part 1 refactor

--- day 16/day16.py
+++ day 16/day16.py
@@ -4,19 +4,29 @@ data = file.read().split("\n\n")
 
 # parsing allowed values
 allowed_values = set()
+# needed for part 2
+fields = {}
 for x in data[0].split("\n"):
+    key = x.split(":")[0]
+    values = set()
     try:
         for num in range(int(x.split(" ")[1].split("-")[0]), int(x.split(" ")[1].split("-")[1]) + 1):
             allowed_values.add(num)
+            values.add(num)
     except ValueError:
         for num in range(int(x.split(" ")[2].split("-")[0]), int(x.split(" ")[2].split("-")[1]) + 1):
             allowed_values.add(num)
+            values.add(num)
     try:
         for num in range(int(x.split(" ")[3].split("-")[0]), int(x.split(" ")[3].split("-")[1]) + 1):
             allowed_values.add(num)
+            values.add(num)
     except ValueError:
         for num in range(int(x.split(" ")[4].split("-")[0]), int(x.split(" ")[4].split("-")[1]) + 1):
             allowed_values.add(num)
+            values.add(num)
+    # for part 2
+    fields[key] = values
 
 # parsing tickets
 ticket_list = []
@@ -35,9 +45,10 @@ for ticket in ticket_list:
         continue
     else:
         # adding the values in ticket that are not included in allowed values to a list
-        print(ticket - allowed_values)
         for j in ticket - allowed_values:
             invalid_values.append(j)
 
 print(sum(invalid_values))
+
+# part 2
 file.close()
