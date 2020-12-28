7619348a934e0301f592b268ed669f7b8eead98b
Daniele Siri <daniele.siri97@gmail.com>
2020-12-28 14:15:45 +0100

    day 15

--- day 15/day15.py
+++ day 15/day15.py
@@ -1,34 +1,44 @@
 # https://adventofcode.com/2020/day/15
+from array import array
+
+
 file = open('day15_data', 'r')
 data = file.read().splitlines()
-game = {}
-starting_numbers = []
 for x in data:
-    starting_numbers = x.split(",")
-for i, v in enumerate(starting_numbers):
-    # i + 1 to synchronize it with the turns
-    game[i + 1] = int(v)
-
-
-def get_turn(n):
-    # get the key for the corresponding value
-    for y, z in game.items():
-        if z == n:
-            return y
-
-
+    starting_numbers = array('b', (int(y) for y in x.split(",")))
+# dict entries = {number: (turn, turn difference between current turn and previous turn when the number occurred)}
+start_game = {v: (i + 1, 0) for i, v in enumerate(starting_numbers[:-1])}
 # initializing the turn number depending on how many starting numbers there are
-turn = len(starting_numbers)
-while 2020 not in game.keys():
-    # first time the number has been spoken
-    if game[turn] not in list(game.values())[:-1]:
-        game[turn + 1] = 0
-    else:
-        previous_turn = get_turn(game[turn])
-        game[turn + 1] = turn - previous_turn
-        # delete oldest value
-        game.pop(previous_turn)
-    turn += 1
+start_turn = len(starting_numbers)
+next_value_start = starting_numbers[-1]
 
-print(game[2020])
+
+def solve(stop, turn, next_value, game):
+    print(game)
+    while True:
+        if turn == stop:
+            return next_value
+        # number already spoken
+        if next_value in game:
+            # assigning the turn difference to the next value
+            next_value = game[next_value][1]
+            # if the value does not exist in game, it would raise a KeyError exception
+            try:
+                game[next_value] = (turn + 1, (turn + 1) - game[next_value][0])
+            except KeyError:
+                game[next_value] = (turn + 1, 0)
+        # first time the number has been spoken
+        else:
+            # first iteration
+            if turn == len(starting_numbers):
+                game[next_value] = (turn, 0)
+            next_value = 0
+            # updating the entry for 0
+            game[0] = (turn + 1, (turn + 1) - game[0][0])
+        turn += 1
+
+
+print(solve(2020, start_turn, next_value_start, start_game.copy()))  # part 1
+print(solve(30000000, start_turn, next_value_start, start_game.copy()))  # part 2
+
 file.close()
